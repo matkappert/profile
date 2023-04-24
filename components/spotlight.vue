5842d10db56762f865bdbd3e530a3640e1afbaaf
Mat Kappert <matkappert@gmail.com>
2023-04-24 10:54:42 +1000

    chore: housekeeping

--- components/spotlight.vue
+++ components/spotlight.vue
@@ -4,8 +4,10 @@ const { x, y } = useMouse();
 
 </script>
 <template>
-  <div
-    class="absolute w-[500] h-[500px] aspect-square rounded-full  -translate-x-1/2 -translate-y-1/2 -z-10 dark:bg-indigo-500/5 bg-violet-400/10 overflow-hidden"
-    :style="`left: ${x}px; top: ${y}px;`">
+  <div class="w-screen h-screen absolute inset-0 overflow-hidden">
+    <div
+      class="absolute w-[500px] h-[500px] aspect-square rounded-full -translate-x-1/2 -translate-y-1/2 -z-10 dark:bg-indigo-500/10 bg-violet-400/10"
+      :style="`left: ${x}px; top: ${y}px;`">
+    </div>
   </div>
 </template>
