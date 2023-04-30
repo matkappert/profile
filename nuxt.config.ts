bc09b5cc066cf00b6a829999dfcc368468bcf1e4
Mat Kappert <matkappert@gmail.com>
2023-04-30 10:54:42 +1000

    chore: housekeeping

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
+// import { createResolver } from '@nuxt/kit';
+// const { resolve } = createResolver(import.meta.url);
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  typescript: {
-    strict: true,
-    typeCheck: true,
-  },
   modules: [
     '@vueuse/nuxt',
     '@nuxtjs/tailwindcss',
@@ -12,17 +11,20 @@ export default defineNuxtConfig({
   ],
   extends: [
     'nuxt-seo-kit',
-    '../nuxt-ui/',
-    // 'github:matkappert/nuxt-ui#main',
+    // '../nuxt-ui/',
+    // 'github:matkappert/nuxt-ui#master',
   ],
   alias: {
-    'class-variance-authority': require.resolve('class-variance-authority'),
+    // 'class-variance-authority': require.resolve('class-variance-authority'),
     // 'tailwindcss': require.resolve('tailwindcss/lib/index.js'),
     // 'tailwindcss/colors': require.resolve('tailwindcss/colors.js'),
+    // 'zod': 'zod',
+    // 'class-variance-authorit': 'class-variance-authorit',
+    // 'tailwindcss': 'tailwindcss',
+    // 'tailwindcss/colors': 'tailwindcss/colors',
   },
   ssr: true,
   nitro: {
-    preset: 'cloudflare_pages',
     devProxy: {
       '/api': {
         target: 'http://localhost:8788/api',
@@ -32,7 +34,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://profile.kappert.workers.dev',
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://matkappert.com',
       siteName: 'Mat Kappert',
       siteDescription: 'Personal profile for software developer and electronics technician Mat Kappert.',
       language: 'en-AU',
